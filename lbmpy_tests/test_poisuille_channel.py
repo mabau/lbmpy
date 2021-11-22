116b46aab39da6f9fcf76fa78461b58b3dcd44a7
Markus Holzer <markus.holzer@fau.de>
2021-11-22 22:30:23 +0000

    Remove OpenCL support

--- lbmpy_tests/test_poisuille_channel.py
+++ lbmpy_tests/test_poisuille_channel.py
@@ -10,15 +10,11 @@ import pystencils as ps
 from poiseuille import poiseuille_channel
 
 
-@pytest.mark.parametrize('target', (ps.Target.CPU, ps.Target.GPU, ps.Target.OPENCL))
+@pytest.mark.parametrize('target', (ps.Target.CPU, ps.Target.GPU))
 @pytest.mark.parametrize('stencil_name', (Stencil.D2Q9, Stencil.D3Q19))
 def test_poiseuille_channel(target, stencil_name):
-    # OpenCL and Cuda
-    if target == ps.Target.OPENCL:
-        import pytest
-        pytest.importorskip("pyopencl")
-        import pystencils.opencl.autoinit
-    elif target == ps.Target.GPU:
+    # Cuda
+    if target == ps.Target.GPU:
         import pytest
         pytest.importorskip("pycuda")
 
