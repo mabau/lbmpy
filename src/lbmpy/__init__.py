f4dd53967be97b36e90a750b2dd7ccd014f3266c
Frederik Hennig <frederik.hennig@fau.de>
2024-05-13 12:56:16 +0200

    Remove vendored versioneer

--- src/lbmpy/__init__.py
+++ src/lbmpy/__init__.py
@@ -55,7 +55,5 @@ __all__ = [
 ]
 
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
